e938a5614256b83dcced38c2547ca6a79cb05f59
kamillbak <kamil1427@o2.pl>
2024-04-02 00:17:59 +0200

    fix

--- src/main/java/com/kamillbak/kafkaexample/config/TopicConfiguration.java
+++ src/main/java/com/kamillbak/kafkaexample/config/TopicConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
-public class KafkaTopicConfig {
+public class TopicConfiguration {
 
     public NewTopic myTopic() {
         return TopicBuilder.name("myTopic").build();
